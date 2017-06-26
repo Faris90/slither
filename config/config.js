5164157f73c9a2d60c173411c75f424495d61ace
( ͡° ͜ʖ ͡°) Fs unlimited <faris.soliman@yahoo.com>
2017-06-26 06:32:54 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
 module.exports = {
     port: 8080,
-    bots: 0, // Not Implemented
+    bots: 10, // Not Implemented
     maxConnections: 100,
 	highscoreName: "How to change Highscore Msg",
 	highscoreMsg: "Set this message in the config",
     food: 20000,
-    foodColours: 8,
-    foodSize: [15, 47],
+    foodColours: 20,
+    foodSize: [15, 47, 200,],
     foodPerSpawn: 1000, // Not Implemented
     gameRadius: 21600,
     sectorSize: 480
